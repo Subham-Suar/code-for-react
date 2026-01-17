a3caa2990915f9aae6c58fc0083333267c584430
Subham-Suar <subhamsuar911609@gmail.com>
2026-01-17 19:48:27 +0530

    tailwind-props cnpt

---
+++ 01_ViteReact/tailwind-props/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const user ={
+    userName:"Subham",
+    age:19
+  }
+
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline bg-amber-500" >
+    Hello world!
+  </h1>
+  <Card username="Subham" text="hi"/>
+    
+    </>
+  )
+}
+
+export default App
